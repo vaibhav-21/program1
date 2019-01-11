cae38d831f9950a0d5aade2af6d93ef4bb061b85
vaibhav sharma <vaibhav.vs0111@gmail.com>
2019-01-11 12:39:06 +0530

    added

---
+++ src/Add.java
@@ -0,0 +1,11 @@
+class Add
+{
+	public static void main(String args[])
+ {
+	int a,b,c;
+	a=100;
+	b=50;
+	c=a+b;
+	System.out.println("a+b" + c);
+  }
+}
---
+++ src/BoolDemo.java
@@ -0,0 +1,13 @@
+class BoolDemo 
+{
+	public static void main(String args[]) 
+	{
+	    boolean b;
+	    b = false;
+	    System.out.println("b is " + b);
+	    b = true;
+	    System.out.println("b is " + b);
+	    // outcome of a relational operator is a boolean value
+	    System.out.println("10 > 9 is " + (10 > 9));
+	}
+}
