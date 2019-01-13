4e4bd4428304a4e2ba0234273cd44cf0dda208dd
vaibhav sharma <vaibhav.vs0111@gmail.com>
2019-01-14 00:45:24 +0530

    added

---
+++ src/BasicOperation.java
@@ -0,0 +1,19 @@
+class BasicOperation
+{
+    public static void main(String args[])
+    {
+       int p=99;
+        int q=1;
+        int oop;
+        oop = p + q;
+       System.out.println("Addition = " +oop);
+     oop = p - q;
+       System.out.println("Subtraction = " +oop);
+      oop = p * q;
+       System.out.println("Multiplication = " +oop);
+     oop = p / q;
+       System.out.println("Division = " +oop);
+       
+       }
+
+       }
---
+++ src/Typeconversion.java
@@ -0,0 +1,9 @@
+class Typeconversion
+{
+public static void main (String args[])
+{ char z='v';
+int a;
+a=(int)z;
+System.out.println("int representation is"+a);
+}
+}
